d986b62587e89c8dfc333077d9fe6af141df42e9
emim54 <156033266+emim54@users.noreply.github.com>
2024-02-09 15:21:50 +0100

    Delete cypress/e2e directory

--- cypress/e2e/figma.cy.js
+++
@@ -1,8 +0,0 @@
-import '@percy/cypress';
-
-describe('Website öffnen', () => {
-  it('Besucht Ikra Bau Startseite', () => {
-    cy.visit('https://ikrabau.de/');
-    cy.percySnapshot('Startseite');
-  });
-});
